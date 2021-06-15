1698b1b6fbaeefe5c54d6832736a1c9f0f6f2c33
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-06-15 08:34:24 -0400

    init commit for a logs parser for madmax app for chia mining

---
+++ readme.md
@@ -0,0 +1,39 @@
+# crypto - data parser
+This script takes the piped input and extracts a pattern.
+
+## Suggested Use
+
+### save your plottting into a log.txt file
+Plotting takes some time and because of that we need to save it to a log fie first.
+```bash
+$ <run your command> &> logs.txt 
+```
+
+Once plotting is complete do the following
+
+```bash
+cat logs.txt | python3 stdin.py
+```
+
+- `&>` redirects the stdout and stderr to your log file
+- `cat <filename>` sends the output to stdout
+- `python3 stdin.py` runs the script
+- `|` pipes are used to direct output as input to next command
+
+### General overview
+> pattern: `Total plot creation time was XXXXX.XX sec`
+
+use test.txt to see this in action
+
+```bash
+$ cat test.txt | python3 stdin.py
+```
+
+### Regex used
+```
+^(Total plot creation time was ([0-9]{1,6}\.?[0-9]{1,2}) sec)
+```
+
+### Pre-Requisites
+[python3](https://www.python.org/downloads/)
+[python pandas](https://pypi.org/project/pandas/) -- run through pip
---
+++ stdin.py
@@ -0,0 +1,39 @@
+import sys
+import re
+# import pandas as pd
+from datetime import datetime as dt
+
+# use this to find out more https://regex101.com/
+# https://regex101.com/r/G2o0h9/1
+PATTERN = r'^(Total plot creation time was ([0-9]{1,6}\.?[0-9]{1,2}) sec)'
+TODAY = dt.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+# create placeholder for files
+total_plot_logs = open(f'logging {TODAY}.txt', 'w')
+numbers_only = open(f'total_plots_time {TODAY}.txt', 'w')
+base_str = ''
+
+# read piped inputs and save them into a base string
+# to be used to extract the pattern
+for line in sys.stdin:
+    base_str = base_str + '\n' + line
+
+# returns an array of tuples
+# elem at index 0 contains full string
+# elem at index 1 contains only num of secs
+all_matches = re.findall(PATTERN, base_str, flags=re.MULTILINE)
+total_matches = len(all_matches)
+
+# save totals
+total_plot_logs.write(f'Total Number Of Matches: {total_matches}\n\n')
+numbers_only.write(f'Total Number Of Matches: {total_matches}\n\n')
+
+# write each find into .txt file
+for match in all_matches:
+    total_plot_logs.write(match[0] + '\n')
+    # number = float(match[1])
+    numbers_only.write(match[1] + '\n')
+
+# close files when everything is written
+total_plot_logs.close()
+numbers_only.close()
